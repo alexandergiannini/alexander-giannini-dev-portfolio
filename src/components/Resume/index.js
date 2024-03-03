63df5354012a7c313d7b730264d61a3a5cf6ecc4
Alexander Giannini <alexgiannini@Alexanders-MBP.lan>
2024-03-03 00:25:44 -0800

    fixed resume button

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
-//import PDF from '../Resume/Alex_Giannini_Resume_(2021).pdf'
-//import coverImage from "../../assets/cover/cover-image.jpg";
 //add an alt attribute to the img src
 import resume from "../../assets/resume/AlexanderGianniniQAResume2024.pdf"
-const resumePDF = 'https://alexandergiannini.github.io/alexander-giannini-dev-portfolio/AlexanderGianniniQAResume2024.pdf'
+//const resumePDF = 'https://alexandergiannini.github.io/alexander-giannini-dev-portfolio/AlexanderGianniniQAResume2024.pdf'
 function Resume() {
-    const downloadFileAtUrl = (url) => {
-        const aTag = document.createElement('a');
-        aTag.href = url
-        aTag.setAttribute('download', 'AlexanderGianniniQAResume2024.pdf')
-        document.body.appendChild(aTag)
-        aTag.click()
-        aTag.remove()
-    }
     return (
       <section className="m-5">
         <h1 id="about">Resume</h1>
-        <a href={resume} download="Resume">Download!!</a>
-        <button onClick={() => {downloadFileAtUrl(resumePDF)}}>Download Resume</button>
-        {/* <a href={require('src/components/Resume/Alex_Giannini_Resume_(2021).pdf')} target="_blank" id="resume-link">Download my resume</a> */}
+        <a href={resume} download="Resume">
+        <button>Download Resume</button>
+        </a>
         <h2>QA Automation Proficiencies</h2>
         <ul>
             <li className="li-proficiences">
